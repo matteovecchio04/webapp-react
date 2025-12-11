00bbef8d145d12e615064f53a6caac7182c162c5
matteovecchio04 <matteovex@gmail.com>
2025-12-11 19:55:03 +0100

    add the mapping of the movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-
-
 export default function Home() {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
-        axios.get("http://localhost:3000")
-            .then(data => {
-                console.log(data);
+        axios.get("http://localhost:3000/movies")
+            .then(res => {
+                setMovies(res.data)
+                console.log(res.data);
             })
             .catch(error => {
                 console.error(error);
@@ -17,6 +16,21 @@ export default function Home() {
     }, [])
 
     return (
-        <h2>Home</h2>
+        <>
+            <h1>Home</h1>
+            {
+                movies.map(movie => {
+                    return (
+                        <div key={movie.id} className="card">
+                            <h1>{movie.title}</h1>
+                            <img src={movie.image} className="card-img-top" alt={movie.title} />
+                            <div className="card-body">
+                                <p className="card-text">{movie.abstract}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
