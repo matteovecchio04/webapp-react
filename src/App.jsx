a8931ec7a4c08374f736d276edea11d4e35f7ede
matteovecchio04 <matteovex@gmail.com>
2025-12-11 18:35:14 +0100

    Add routing and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
+import { MovieCard } from "./components/MovieCard"
+import { Layout } from "./layout/Layout"
 
 export default function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />} >
 
+            <Route path='/' element={ } />
+            <Route path='/boh' element={ } />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
